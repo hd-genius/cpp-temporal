7702fa16c767ab2d583a06d943051d1fc74eaf14
Roy Stewart <roythomasstewart@gmail.com>
2022-12-10 23:08:45 -0500

    Added remaining TempoalUnit::of cases

--- src/temporal-unit/temporal-unit.cpp
+++ src/temporal-unit/temporal-unit.cpp
@@ -7,8 +7,53 @@ TemporalUnit& TemporalUnit::of(ChronoUnit unit) {
 	TemporalUnit*& temporalUnit = unitCache[unit];
 	if (temporalUnit == nullptr) {
 		switch (unit) {
+		case NANOS:
+			temporalUnit = new TemporalUnit(NANOS, false, true, false);
+			break;
+		case MICROS:
+			temporalUnit = new TemporalUnit(NANOS, false, true, false);
+			break;
+		case MILLIS:
+			temporalUnit = new TemporalUnit(NANOS, false, true, false);
+			break;
 		case SECONDS:
 			temporalUnit = new TemporalUnit(SECONDS, false, true, false);
+			break;
+		case MINUTES:
+			temporalUnit = new TemporalUnit(MINUTES, false, true, false);
+			break;
+		case HOURS:
+			temporalUnit = new TemporalUnit(HOURS, false, true, false);
+			break;
+		case HALF_DAYS:
+			temporalUnit = new TemporalUnit(HALF_DAYS, false, true, false);
+			break;
+		case DAYS:
+			temporalUnit = new TemporalUnit(DAYS, true, false, false);
+			break;
+		case WEEKS:
+			temporalUnit = new TemporalUnit(WEEKS, true, false, false);
+			break;
+		case MONTHS:
+			temporalUnit = new TemporalUnit(MONTHS, true, false, true);
+			break;
+		case YEARS:
+			temporalUnit = new TemporalUnit(YEARS, true, false, true);
+			break;
+		case DECADES:
+			temporalUnit = new TemporalUnit(DECADES, true, false, true);
+			break;
+		case CENTURIES:
+			temporalUnit = new TemporalUnit(CENTURIES, true, false, true);
+			break;
+		case MILLENIA:
+			temporalUnit = new TemporalUnit(MILLENIA, true, false, true);
+			break;
+		case ERAS:
+			temporalUnit = new TemporalUnit(ERAS, true, false, true);
+			break;
+		case FOREVER:
+			temporalUnit = new TemporalUnit(FOREVER, true, false, true);
 			break;
 		}
 	}
